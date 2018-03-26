be42ef26fc38e6e3c8f38a5b0e5e189b65b6aa1e
Dariusz Franczak <darius32@op.pl>
2018-03-27 00:05:53 +0100

    gulp configured

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+var sassSourceFile = 'source/style.scss';
+var cssCopmiledFile = 'style';
+var watchedFiles = 'source/**/*.scss';
+
+gulp.task('buildcss', function(){
+	return gulp.src(sassSourceFile)
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(gulp.dest(cssCopmiledFile));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(watchedFiles, ['buildcss']);
+})
